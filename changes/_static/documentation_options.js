2501e0cde27d7083577019aaa8d8af3429e20fd7
shihab-dls <shihab-dls@users.noreply.github.com>
2024-03-28 12:22:27 +0000

    deploy: 0e2dfc22897465533e80eb82cf073bcb19d33990

---
+++ changes/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.1a2.dev33+g0e2dfc2',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
